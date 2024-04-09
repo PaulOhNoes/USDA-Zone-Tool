e4cafe596372870a77ebf7685cd45c89c98c2b95
PaulOhNoes <paul@colorblends.com>
2024-04-09 12:50:13 -0400

    update

--- index.ts
+++ index.ts
@@ -1,27 +1,27 @@
 import ZoneTable from "./ZoneTable";
 
-const Scenario1 = () => {
-  ZoneTable.loadDataSync();
-  const data = ZoneTable.getZipCodeData("10001");
-  console.log("My zone is", data.zone);
-};
+// const Scenario1 = () => {
+//   ZoneTable.loadDataSync();
+//   const data = ZoneTable.getZipCodeData("10001");
+//   console.log("My zone is", data.zone);
+// };
 
-const Scenario2 = async () => {
-  await ZoneTable.loadData();
-  const data = ZoneTable.getZipCodeData("10001");
-  console.log("My zone is", data.zone);
-};
+// const Scenario2 = async () => {
+//   await ZoneTable.loadData();
+//   const data = ZoneTable.getZipCodeData("10001");
+//   console.log("My zone is", data.zone);
+// };
 
-const GenerateJSONScenario = () => {
-  if (!ZoneTable.isDataLoaded()) {
-    ZoneTable.loadDataSync();
-  }
+// const GenerateJSONScenario = () => {
+//   if (!ZoneTable.isDataLoaded()) {
+//     ZoneTable.loadDataSync();
+//   }
 
-  ZoneTable.generateJson();
-};
+//   ZoneTable.generateJson();
+// };
 
-Scenario1();
+// Scenario1();
 // Scenario2();
 // GenerateJSONScenario();
 
-export default ZoneTable;
+module.exports = ZoneTable;
