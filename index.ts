2623e690fe441e0d8d1759e804f48f5e1a111d0e
PaulOhNoes <paul@colorblends.com>
2024-04-09 13:10:29 -0400

    update

--- index.ts
+++ index.ts
@@ -1,29 +1,97 @@
-import ZoneTable from "./ZoneTable";
+import { ZipData, Zipcodes } from "./types";
+import {
+  createJSONFile,
+  processFileLines,
+  processFileLinesSync,
+} from "./utils";
 
-// const Scenario1 = () => {
-//   ZoneTable.loadDataSync();
-//   const data = ZoneTable.getZipCodeData("10001");
-//   console.log("My zone is", data.zone);
-// };
+export class ZoneTable {
+  static data: Zipcodes;
 
-// const Scenario2 = async () => {
-//   await ZoneTable.loadData();
-//   const data = ZoneTable.getZipCodeData("10001");
-//   console.log("My zone is", data.zone);
-// };
+  /**
+   * Reads the entire file first before constructing the dataset
+   *
+   * Use `loadDataSync()` for a less resource intensive operation
+   *
+   * @returns void
+   */
+  static loadDataSync() {
+    ZoneTable.data = processFileLinesSync();
+  }
 
-// const GenerateJSONScenario = () => {
-//   if (!ZoneTable.isDataLoaded()) {
-//     ZoneTable.loadDataSync();
-//   }
+  /**
+   * Reads the file in a stream before constructing the dataset
+   *
+   * Requires `async`/`promise` handling
+   *
+   * @returns void
+   */
+  static async loadData() {
+    ZoneTable.data = await processFileLines();
+  }
 
-//   ZoneTable.generateJson();
-// };
+  /**
+   * Generates a json file under data folder
+   *
+   * @returns void
+   */
+  static generateJson = () => {
+    try {
+      createJSONFile(ZoneTable.data);
+    } catch (error) {
+      console.log("Could not create json file", error);
+    }
+  };
 
-// Scenario1();
-// Scenario2();
-// GenerateJSONScenario();
+  /**
+   * get zone data via zipcode
+   * @param zipcode 5 digit zipcode
+   * @returns an object containg zone, trange and zonetitle
+   */
+  static getZipCodeData = (zipcode: string | number): ZipData => {
+    let input = zipcode;
+    if (typeof input === "number") {
+      input = zipcode.toString();
+    }
 
-module.exports = {
-  ZoneTable: ZoneTable,
-};
+    if (input.length !== 5) {
+      throw Error("Not a valid zipcode.");
+    }
+
+    const result = ZoneTable.data[input];
+
+    if (!result) {
+      throw Error("No data found.");
+    }
+
+    return result;
+  };
+
+  /**
+   * return true if data has values
+   *
+   * @returns true or false
+   */
+  static isDataLoaded = (): boolean => {
+    if (
+      typeof ZoneTable.data !== null &&
+      typeof ZoneTable.data === "object" &&
+      Object.keys(ZoneTable.data).length > 0
+    ) {
+      return true;
+    }
+
+    return false;
+  };
+
+  /**
+   * deletes the data set
+   *
+   * @returns void
+   */
+  static deleteData = () => {
+    ZoneTable.data = {};
+
+    return false;
+  };
+}
